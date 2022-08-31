47c5c48587a019cb18657c8c20c9a5e63cc7f100
AJTrapper <ajtucker@ualr.edu>
2022-08-31 14:55:56 +0000

    desks

---
+++ 01.07 School Desks.py
@@ -0,0 +1,14 @@
+a = int(input("Enter Classroom A: "))
+b = int(input("Enter Classroom B: "))
+c = int(input("Enter Classroom C: "))
+d = int(a)//2
+e = int(a)%2 
+f = int(d)+int(e)
+g = int(b)//2
+h = int(b)%2 
+i = int(g)+int(h)
+j = int(c)//2
+k = int(c)%2 
+l = int(j)+int(k)
+m= int(f)+int(i)+int(l)
+print(m)
