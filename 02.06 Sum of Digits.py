6f62ecc89340219d86423213191ec2e6e27ce35d
AJTrapper <ajtucker@ualr.edu>
2022-09-07 16:33:52 +0000

    sum

--- 02.06 Sum of Digits.py
+++ 02.06 Sum of Digits.py
@@ -1,15 +1,7 @@
 a = int(input("Enter a Number :"))
 b = a//1000
-#finds 1000's 
-
 c = (a%1000)//100
-#finds 100's digit
-
 d = (a%100)//10
-#finds 10's digit
-
 e = (a%10)//1
-#finds 1's digit
-
 f = int(b)+int(c)+int(d)+int(e)
 print(f)
