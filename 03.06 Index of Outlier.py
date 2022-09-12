be300af2cc36d8a4e1fb6d269d37fe15f2970d8b
AJTrapper <ajtucker@ualr.edu>
2022-09-12 22:31:05 +0000

    outlier

---
+++ 03.06 Index of Outlier.py
@@ -0,0 +1,12 @@
+x = int(input("Enter First Number :"))
+y = int(input("Enter Second Number :"))
+z = int(input("Enter Third Number :"))
+
+if x != y and x != z:
+    print(1)
+
+elif y != x and y != z:
+    print(2)
+
+else :
+    print (3)
