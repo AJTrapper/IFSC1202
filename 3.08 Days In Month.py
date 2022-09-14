232515037f2a06afeda56bfa92f2e88b93dfb55c
AJTrapper <ajtucker@ualr.edu>
2022-09-14 00:35:05 +0000

    days and months

--- 3.08 Days In Month.py
+++ 3.08 Days In Month.py
@@ -1,11 +1,11 @@
-x = int(input("Enter a Month :"))
+x = int(input("Enter a Month : "))
 # 30 days = 4,6,9,11
 # 31 days =1,3,5,7,8,10,12
 
-if x < y and x < z:
+if x == 4 or x == 6 or x == 9 or x == 11:
     print("30 Days")
 
-elif y < x and y < z:
+elif x == 1 or x == 3 or x == 5 or x == 7 or x == 8 or x == 10 or x == 12:
     print("31 Days")
 
 else :
