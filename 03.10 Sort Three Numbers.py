6df598a0b7bfa8f420d3ef199214b6cad19a6f50
AJTrapper <ajtucker@ualr.edu>
2022-09-14 14:39:11 +0000

    ascending number

--- 03.10 Sort Three Numbers.py
+++ 03.10 Sort Three Numbers.py
@@ -1,20 +1,20 @@
 x = int(input("First number : "))
 y = int(input("Second number : "))
 z = int(input("Third Number : "))
-if x>y and x>z:
-    if y>z:
+if x<y and x<z:
+    if y<z:
         print(x,y,z)
     else:
         print(x,z,y)    
 
-elif y>x and y>z:
-    if x>z:
+elif y<x and y<z:
+    if x<z:
         print(y,x,z)
     else:
         print(y,z,x)   
 
 else :
-    if x>y:
+    if x<y:
         print(z,x,y)
     else:
         print(z,y,x)   
