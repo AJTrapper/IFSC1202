f5a51f1b8d37a4c80d2cf0cd78d015dcde7a58ff
AJTrapper <ajtucker@ualr.edu>
2022-09-21 01:19:54 +0000

    ladder

---
+++ 04.07 Ladder.py
@@ -0,0 +1,5 @@
+x = int(input("Enter A :"))
+for a in range(1,x+1):
+    for i in range(1, a+1):
+        print(i, end='')
+    print('')
---
+++ 4.06 Squares In a Range.py
@@ -0,0 +1,4 @@
+x = int(input("Enter A :"))
+y = int(input("Enter B :"))
+for i in range(x,(y+1)):
+    print(i**2)
