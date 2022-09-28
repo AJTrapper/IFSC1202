7c46695cb4bf954d549a4d15f624d98fd75c3fd3
AJTrapper <ajtucker@ualr.edu>
2022-09-28 15:18:44 +0000

    stock

--- 06.02 Stock.py
+++ 06.02 Stock.py
@@ -1,4 +1,16 @@
+
+
+def percentchange (t,y):
+    c = (t-y)/y*100
+    return c
+
 stockfile = open("06.02 Stock.txt")
+x = stockfile.readline()
+y = float(x)
+print("{:>15s} {:>15s}".format("Stock", "Change"))
+print("{:>15.2f}".format(float(x)))
+x = stockfile.readline()  
 while x != "":
-    print(x)
+    print("{:15.2f} {:15.2f}".format(float(x), percentchange(float(x), y)))
+    y = float(x)
     x = stockfile.readline()
