b048087d6646fe9f2dfc9784298f7d82c6f73ac8
AJTrapper <ajtucker@ualr.edu>
2022-10-26 19:32:51 +0000

    checkers

--- 09.01 Maximim Of A Two Dimensional List.py
+++ 09.01 Maximim Of A Two Dimensional List.py
@@ -1,1 +1,8 @@
-x = int(input("Enter the number of rows and columns :"))
+x = input("Enter the number of rows and columns: ")
+y = x.split()
+rows = int(y[0])
+columns = int(y[1])
+a = []
+
+for i in range(0,rows-1):
+    x = input("Enter a line of data: ")
