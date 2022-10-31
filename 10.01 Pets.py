d3e046f89997e95b78edb52c8fcc7e6441c5bc23
AJTrapper <ajtucker@ualr.edu>
2022-10-31 15:50:22 +0000

    name

---
+++ 10.01 Pets.py
@@ -0,0 +1,14 @@
+# Step 1 - Define the class object "Pet"
+class Pet ():
+	
+# Step 2 - Define the initializer and any default values
+    def __init__(self):
+
+# Step 3 - Define the object attributes
+        self.PetName = ""
+        self.PetType = ""
+        self.PetAge = 0
+PetInfo = open("10.01 Pets.txt")
+x = PetInfo.readline()
+y = x.split.PetInfo(",")
+
