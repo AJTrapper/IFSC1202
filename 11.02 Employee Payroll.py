4861ee13e8a8c945729af9b8316d3d27e722b03b
AJTrapper <ajtucker@ualr.edu>
2022-11-16 16:51:24 +0000

    save plz

--- 11.02 Employee Payroll.py
+++ 11.02 Employee Payroll.py
@@ -4,19 +4,18 @@ class Employee ():
     def __init__(self, FirstName, LastName, IDNumber, Wage):
 
 
-        self.employeeName = FirstName
+        self.employeeFirstName = FirstName
         self.employeeLastName = LastName
         self.employeeIDNumber = IDNumber
         self.employeeHoursWorked = 0.0
         self.employeeWage = Wage
     
     def WeeklyPay(self):
-        if self.HoursWorked > 40:
-            x = ((self.HoursWorked -40) *(1.5*self.Wage)) + (40*self.Wage)
+        if self.employeeHoursWorked > 40:
+            x = ((self.employeeHoursWorked-40) *(1.5*self.employeeWage)) + (40*self.employeeWage)
         else:
-            x = (self.HoursWorked)* (self.Wage)
-        self.WeeklyPay = x
-        return self.WeeklyPay
+            x = (self.employeeHoursWorked)* (self.employeeWage)
+        return x
 
 def find_employee(EMPList, EMPID):
     for i in range(len(EMPList)):
@@ -24,13 +23,12 @@ def find_employee(EMPList, EMPID):
             return i
     return -1
 
-
 EmployeeLists = []
 EmployeeFile = open("11.02 Employees.txt")
 x = EmployeeFile.readline()
 while x != "":
     y = x.split(",")
-    MyEmployee = Employee(y[0], y[1], int(y[2]), float(y[3]))
+    MyEmployee = Employee(y[0].strip(), y[1].strip(), int(y[2]), float(y[3]))
     EmployeeLists.append(MyEmployee)
     x = EmployeeFile.readline()
 EmployeeFile.close()
@@ -40,11 +38,14 @@ x = EmployeeHours.readline()
 while x != "":
     y = x.split(",")
     employeeIDNumber = int((y[0]).strip())
-    employeeHoursWorked = float(y[1]).strip()
+    employeeHoursWorked = float((y[1]).strip())
     empidx = find_employee(EmployeeLists, employeeIDNumber)
     EmployeeLists[empidx].employeeHoursWorked = employeeHoursWorked
+    x = EmployeeHours.readline()
 EmployeeHours.close()    
 
 print("{:>10s} {:>10s} {:>10s} {:>10} {:>10s} {:>10}".format( "First", "Last", "ID", "Hours", "Hourly", "Weekly"))
 print("{:>10s} {:>10s} {:>10s} {:>10} {:>10s} {:>10}".format( "Name", "Name", "Number", "Worked", "Wage", "Pay"))
 
+for i in range (len(EmployeeLists)):
+    print("{:>10s} {:>10s} {:>10d} {:>10.2f} {:10.2f} {:10.2f}".format(EmployeeLists[i].employeeFirstName, EmployeeLists[i].employeeLastName, EmployeeLists[i].employeeIDNumber, EmployeeLists[i].employeeHoursWorked, EmployeeLists[i].employeeWage, EmployeeLists[i].WeeklyPay()))
