36c8f41cfa1bee1aca950bdb59f320f68326b26d
AJTrapper <ajtucker@ualr.edu>
2022-11-16 21:34:26 +0000

    save

--- 11.01 Retail Item.py
+++ 11.01 Retail Item.py
@@ -6,5 +6,32 @@ class Retailitem ():
         self.itemDescription = Description
         self.itemUnitsOnHand = UnitsOnHand
         self.itemPrice = Price
+    
     def IventoryValue(self):
-        x = 
+        x = (self.itemUnitsOnHand)*(self.itemPrice)
+        return x
+
+
+print_inventory = []
+RetailFile = open("11.01 Inventory.txt")
+x = ReadFile.readline()
+while x !="":
+    y = x.split(",")
+    MyRetail= Retail(y[0].strip(), y[1].strip(), int(y[2]), float(y[3]))
+    print_inventory.
+
+
+EmployeeLists = []
+EmployeeFile = open("11.02 Employees.txt")
+x = EmployeeFile.readline()
+while x != "":
+    y = x.split(",")
+    MyEmployee = Employee(y[0].strip(), y[1].strip(), int(y[2]), float(y[3]))
+    EmployeeLists.append(MyEmployee)
+    x = EmployeeFile.readline()
+EmployeeFile.close()
+
+
+
+
+print("{:>10s} {:>10s} {:>10s} {:>10}".format( "Description", "Units On Hand", "Price", "Inventory Value"))
