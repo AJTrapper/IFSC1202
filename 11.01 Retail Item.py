eba7839b02d8f0cb91d8a88d64544da6b03d599d
AJTrapper <ajtucker@ualr.edu>
2022-11-16 19:32:42 +0000

    save

---
+++ 11.01 Retail Item.py
@@ -0,0 +1,10 @@
+class Retailitem ():
+
+    def __init__(self, Description, UnitsOnHand, Price):
+
+
+        self.itemDescription = Description
+        self.itemUnitsOnHand = UnitsOnHand
+        self.itemPrice = Price
+    def IventoryValue(self):
+        x = 
