45fc1a4d4e445a98119c9da41581c2fb5f54fd6d
AJTrapper <ajtucker@ualr.edu>
2022-11-16 02:20:22 +0000

    sane

--- 11.02 Employee Payroll.py
+++ 11.02 Employee Payroll.py
@@ -33,17 +33,18 @@ while x != "":
     MyEmployee = Employee(y[0], y[1], int(y[2]), float(y[3]))
     EmployeeLists.append(MyEmployee)
     x = EmployeeFile.readline()
+EmployeeFile.close()
 
 EmployeeHours = open("11.02 Hours.txt")
 x = EmployeeHours.readline()
 while x != "":
     y = x.split(",")
-    employeeIDNumber = int(y[0])
-    employeeHoursWorked = float(y[1])
+    employeeIDNumber = int((y[0]).strip())
+    employeeHoursWorked = float(y[1]).strip()
     empidx = find_employee(EmployeeLists, employeeIDNumber)
     EmployeeLists[empidx].employeeHoursWorked = employeeHoursWorked
-    
+EmployeeHours.close()    
 
-print("{:>10s} {:>10s} {:>10s} {:>10} {:>10s} {:>10}".format( "First", "Last", "ID", "Hours", "Houly", "Weekly"))
+print("{:>10s} {:>10s} {:>10s} {:>10} {:>10s} {:>10}".format( "First", "Last", "ID", "Hours", "Hourly", "Weekly"))
 print("{:>10s} {:>10s} {:>10s} {:>10} {:>10s} {:>10}".format( "Name", "Name", "Number", "Worked", "Wage", "Pay"))
 
