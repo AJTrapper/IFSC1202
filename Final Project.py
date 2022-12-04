48a6c6db9ee854addc9ed1f73cc1aaf6172498a4
AJTrapper <ajtucker@ualr.edu>
2022-12-04 20:14:10 +0000

    Sve

---
+++ Final Project.py
@@ -0,0 +1,34 @@
+n = int(input("Enter the number of rows and columns: "))
+
+a = []
+for i in range(21):
+    a.append([' '] * (n+2))
+for i in range(1,n+1):
+# left column
+	a[i][0] = '|'
+# right column
+	a[i][n+1] = '|'
+##  upper row
+	a[0][i] = '-'
+# lower row	
+	a[n+1][i] = '-'
+# Corners
+a[0][0]="+"
+a[0][22] ="+"
+a[22][0]="+"
+a[22][22]="+"
+
+for i in range(1,n+1,2):
+    for j in range(1,n+1,2):
+        a[i][j] = "*"
+    
+
+for i in range(2,n+1,2):
+    for j in range(2,n+1,2):
+        a[i][j] = "*"
+    
+
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        print(a[i][j], end=' ')
+    print()
