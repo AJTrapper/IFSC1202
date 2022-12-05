ecb2f3b63c420211bcecb37926731aadcec64fcc
AJTrapper <ajtucker@ualr.edu>
2022-12-05 16:24:31 +0000

    save

--- Final Project.py
+++ Final Project.py
@@ -1,24 +1,24 @@
 class Canvas():
 
-def __init__(self, size, xpos, ypos, direction, pen, canvas):
-    self.size = 20
-    self.xpos= 0 
-    self.ypos = 0
-    self.direction = "U"
-    self.pen = "U"
-    self.canvas = [0,0]
+    def __init__(self, size, xpos, ypos, direction, pen, canvas):
+        self.size = 20
+        self.xpos= 0 
+        self.ypos = 0
+        self.direction = "U"
+        self.pen = "U"
+        self.canvas = [0,0]
 
-def printsketch(self):
-    CanvasList = []
-    CanvasRead = open("Cshape.txt")
+    def printsketch(self):
+
+CanvasList = []
+CanvasRead = open("Cshape.txt")
+x = CanvasRead.readline()
+while x != "":
+    y = x.split(",")
+    canvas = Canvas()
+    CanvasList.append(canvas)
     x = CanvasRead.readline()
-    while x != "":
-        y = x.split(",")
-        canvas = Canvas()
-        CanvasList.append(canvas)
-        x = CanvasRead.readline()
     CanvasRead.close()
-
     print(xpos, ypos, direction)
     
 def penup():
@@ -28,8 +28,11 @@ def pendown():
     content 
 
 def turnleft():
+    content
 
 def turnright():
+    content
 
 def move():
+    content
 
